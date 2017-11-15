846e48c1041cf3fc801c30436ff2c836feddb69d
ethornbury <emer.thornbury@ncirl.ie>
2017-11-15 18:17:19 +0000

    test first commit

---
+++ ActivityforResult1/app/src/main/java/com/nci/asahni/activityforresult1/MainActivity.java
@@ -0,0 +1,41 @@
+package com.nci.asahni.activityforresult1;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView1;
+    Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView1 = (TextView) findViewById(R.id.textView1);
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivityForResult(intent, 2);// Activity is started with requestCode 2
+            }
+        });
+    }
+
+    // Call Back method  to get the Message form other Activity
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // check if the request code is same as what is passed  here it is 2
+        if (requestCode == 2) {
+            String message = data.getStringExtra("MESSAGE");
+            textView1.setText(message);
+        }
+    }
+}
